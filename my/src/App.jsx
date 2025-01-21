ab11707bd3e1b7801b39a96ce29138cce195cd9b
shahbaz12243 <shahbazz12243@gmail.com>
2025-01-21 15:09:01 +0530

    login page created

---
+++ my/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import {Login} from './Component/login'
+
+function App() {
+  
+  return (
+    <>
+       <Routes>
+        <Route path ="/login" element={<Login />} />
+       </Routes>
+     
+    </>
+  )
+}
+
+export default App
