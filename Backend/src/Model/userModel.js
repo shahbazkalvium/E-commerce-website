3fee4ce10beb0ca0640e6055859b72e13127a6e2
shahbaz12243 <shahbazz12243@gmail.com>
2025-01-23 16:17:02 +0530

    done

---
+++ Backend/src/Model/userModel.js
@@ -0,0 +1,81 @@
+const {model, Schema} = require('mongoose');
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: [true, "Please enter your name!"],
+      },
+      email:{
+        type: String,
+        required: [true, "Please enter your email!"],
+      },
+      password:{
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [4, "Password should be greater than 4 characters"],
+        select: false,
+      },
+      phoneNumber:{
+        type: Number,
+      },
+      addresses:[
+        {
+          country: {
+            type: String,
+          },
+          city:{
+            type: String,
+          },
+          address1:{
+            type: String,
+          },
+          address2:{
+            type: String,
+          },
+          zipCode:{
+            type: Number,
+          },
+          addressType:{
+            type: String,
+          },
+        }
+      ],
+      role:{
+        type: String,
+        default: "user",
+      },
+      avatar:{
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+     },
+     createdAt:{
+      type: Date,
+      default: Date.now(),
+     },
+     resetPasswordToken: String,
+     resetPasswordTime: Date,
+    role
+    :{
+        type:String,
+        enum:['user','admin'],
+        default:'user'
+    },
+    avatar:{
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+    }
+})
+const userModel = model('User', userSchema);
+module.exports = userModel;
+
