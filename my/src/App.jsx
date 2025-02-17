38091faeac786259841a9b6232a36757c005c6cc
shahbaz12243 <shahbazz12243@gmail.com>
2025-02-17 14:45:31 +0530

    done

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,25 +1,31 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Login from './Components/Login'
-import { Signup } from './Components/Signup'
+import { Login } from './Component/login'
+import { Signup } from './Component/Signup'
 import { Home } from './page/Home'
-import { ProductForm } from './Components/Productform'
-import { Productcardseller } from './Components/productcardforseller'
+import Navbar from './Component/Navbar'
+import Singlecard from './Component/Singlecard'
+import Productform from './Component/Productform'
 
 
 function App() {
- 
- return (
+  
+
+  return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/Login" element={<Login/>}/>
-      <Route path="/Signup" element={<Signup/>}/>
-   <Route path='/Productform' element={<ProductForm/>}/>
-   <Route path='/my-product' element={<Productcardseller/>}/>
-    </Routes>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Productform" element={<Productform />} />
+       <Route path='/Product/:id' element={<Singlecard/>}/>
+      </Routes>
     </>
-    
   )
 }
 
