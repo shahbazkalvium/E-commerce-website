1ece40eb0fe0c24a6f3422995d58c22ca39bb107
shahbazkalvium <shahbazz12243@gmail.com>
2025-03-17 12:05:04 +0530

    user.js

--- Backend/src/Controllers/user.js
+++ Backend/src/Controllers/user.js
@@ -1,24 +1,65 @@
+
 const {Router}= require("express");
 const userModel = require("../Model/userModel");
+const bcrypt = require("bcrypt");
+const { upload } = require("../../multer");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({path:"./src/config/.env"});
 
-const router = Router();
+const secret = process.env.private_key;
+
+const userrouter = Router();
 
 
-router.post("/create-user",upload.single("file"), async(req,res)=>{
+userrouter.post("/create-user",upload.single("file"), async(req,res)=>{
     const {name, email, password} = req.body;
     const userEmail = await userModel.findOne({email});
     if (userEmail) {
         return next(new ErrorHandler("User already exists", 400));
       }
-const filename = req.file.filename ;
-const fileUrl = path.join(filename);
-const user={
-    name:name,
-    email:email,
-    password:password,
-    avatar: fileUrl,
-} ;
-console.log(user);
+
+      const filename = req.file.filename ; 
+      const fileUrl = path.join(filename);
+
+      bcrypt.hash(password, 10, async function(err, hash) {
+      
+      await userModel.create({
+        name:name,
+        email:email,
+        password: hash,
+        // avatar:fileUrl
+      })
+    })
+
+
 });
 
-module.exports = router;
+userrouter.post("/login", async(req,res)=>{
+    const {email, password} = req.body;
+    const check=await userModel.findOne({email:email});
+    console .log(check);
+
+    if (!check) {
+        return res.status(400).json({message:"User not found"});
+    }
+    bcrypt.compare(password,check.password ,function(err, result) {
+      if(err){
+        return res.status(400).json({message:"Invalid bcrypt compare"});
+      }
+      if(result){
+        jwt.sign({email:email},xyz,(err,token)=>{
+          if(err){
+            return res.status(400).json({message:"Invalid jwt"});
+          }
+          console.log(token);
+          res.status(200).json({token:token});
+        })
+      } else {
+        return res.status(400).json({message:"Invalid password"});
+      }
+
+      });
+})
+
+
+module.exports = userrouter;
